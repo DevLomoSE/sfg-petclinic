5db87cd24d24a19cb728ca54649abe87520bffc3
DevLomoSE <jonathancggs@gmail.com>
2021-09-01 17:19:33 -0500

    FEAT: adding basenetity to the models

--- pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.devlomose.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.devlomose.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
