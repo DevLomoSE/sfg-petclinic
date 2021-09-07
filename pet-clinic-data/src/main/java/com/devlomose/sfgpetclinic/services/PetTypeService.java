e762429ba2a19a220d08764c1ad4733eac448819
DevLomoSE <jonathancggs@gmail.com>
2021-09-07 10:52:19 -0500

    :recycle: PetType Service
    implementing interface and its implementation of PetTypeServiceMap

---
+++ pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package com.devlomose.sfgpetclinic.services;
+
+import com.devlomose.sfgpetclinic.model.PetType;
+
+/**
+ * PetTypeService at: pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services
+ * Created by @DevLomoSE at 7/9/21 10:44.
+ */
+public interface PetTypeService extends CRUDService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,37 @@
+package com.devlomose.sfgpetclinic.services.map;
+
+import com.devlomose.sfgpetclinic.model.PetType;
+import com.devlomose.sfgpetclinic.services.PetTypeService;
+
+import java.util.Set;
+
+/**
+ * PetTypeServiceMap at: pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services/map
+ * Created by @DevLomoSE at 7/9/21 10:45.
+ */
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
