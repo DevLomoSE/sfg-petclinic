ac684b581d47da535284c9e32c8173c3ab836165
DevLomoSE <jonathancggs@gmail.com>
2021-09-07 11:53:23 -0500

    :sparkles: Speciality Service
    adding service map for speciliaty entity

---
+++ pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,10 @@
+package com.devlomose.sfgpetclinic.services;
+
+import com.devlomose.sfgpetclinic.model.Speciality;
+
+/**
+ * SpecialitiesService at: pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services
+ * Created by @DevLomoSE at 7/9/21 11:38.
+ */
+public interface SpecialitiesService extends CRUDService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,39 @@
+package com.devlomose.sfgpetclinic.services.map;
+
+import com.devlomose.sfgpetclinic.model.Speciality;
+import com.devlomose.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * SpecialityServiceMap at: pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services/map
+ * Created by @DevLomoSE at 7/9/21 11:48.
+ */
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
