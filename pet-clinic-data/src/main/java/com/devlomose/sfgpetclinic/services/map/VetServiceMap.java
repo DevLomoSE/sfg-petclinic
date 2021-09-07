85e4c79bd1ae8f1488ce14d1c11366d8a7248c7c
DevLomoSE <jonathancggs@gmail.com>
2021-09-07 13:08:40 -0500

    :recycle: Vet Service
    adding functionality to set id to the speciality of the vet

--- pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/devlomose/sfgpetclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.devlomose.sfgpetclinic.services.map;
 
+import com.devlomose.sfgpetclinic.model.Speciality;
 import com.devlomose.sfgpetclinic.model.Vet;
+import com.devlomose.sfgpetclinic.services.SpecialtyService;
 import com.devlomose.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -21,6 +29,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if(object.getSpecialities().size() > 0){
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpecialty = specialtyService.save(speciality);
+                    speciality.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
